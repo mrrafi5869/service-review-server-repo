ecb9c84b129599c8b52cd2f53e2d9d590b0a78d9
Mohammad Rafe <mrrafi5869@gmail.com>
2022-11-09 13:51:25 +0600

    single Service

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
+
+const services = require('./service.json');
+
+app.get("/", (req, res) => {
+    res.send("service review is running");
+})
+
+app.get("/service", (req, res) => {
+    res.send(services);
+});
+
+app.get('/service/:id', (req, res) => {
+    const id = req.params.id;
+    const service = services.find(singleService => singleService.id == id)
+    res.send(service);
+})
+
+app.listen(port, () => {
+    console.log("service running in port", port);
+})
